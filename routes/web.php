c76ea6c09495c79cd9739506f03b446e4bf95b16
james Lee <unpobby@nate.com>
2023-05-27 12:01:57 +0900

    Admin Category Create

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-
+use App\Http\Livewire\Admin\AdminAddCategoryComponent;
+use App\Http\Livewire\Admin\AdminCategoriesComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\HomeComponent;
@@ -54,6 +55,8 @@ Route::middleware(['auth'])->group(function() {
 
 Route::middleware(['auth','authadmin'])->group(function(){
     Route::get('/admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
+    Route::get('/admin/categories', AdminCategoriesComponent::class)->name('admin.categories');
+    Route::get('/admin/category/add', AdminAddCategoryComponent::class)->name('admin.category.add');
 });
 
 
